2fec90b2e108dc6fc990f66961ed53feceef3fea
Abdiel Martinez <aabdiel1998@gmail.com>
2020-09-07 22:45:30 -0600

    💄 Style example page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const index = () => {
-	return (
-		<div>
-			Webapp Working! 🎉
-		</div>
-	);
-};
+const index = () => (
+	<Wrapper>
+		<h1>
+			App working!
+			<span role='img' aria-label='tada'>🎉</span>
+		</h1>
+	</Wrapper>
+);
 
+const Wrapper = styled.main`
+background: linear-gradient(to right, #007991, #78ffd6);
+position:absolute;
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+height:100%;
+
+color: white;
+`;
 export default index;
