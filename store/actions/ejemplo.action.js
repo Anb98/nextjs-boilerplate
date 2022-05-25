5d25aeb38ced787feab598d48358b0cefcc87eb4
Abdiel Martinez <aabdiel1998@gmail.com>
2022-05-24 23:20:03 -0600

    ➖ removed redux

--- store/actions/ejemplo.action.js
+++
@@ -1,3 +0,0 @@
-export const SET_EJEMPLO = 'SET_EJEMPLO';
-
-export const setEjemplo = (payload) => ({ type: SET_EJEMPLO, payload });
--- store/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import ejemplo from './reducers/ejemplo.reducer';
-
-const rootReducer = combineReducers({
-	ejemplo
-});
-
-export default rootReducer;
